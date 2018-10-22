b1a05a693bfa88dd87d7503ce11f3ff9c8610291
Steve Ravet <steve.ravet@apple.com>
2018-10-21 22:23:47 -0500

    Initial Commit

---
+++ glocraft_tabbed/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  glocraft_tabbed
+//
+//  Created by Steve Ravet on 10/21/18.
+//  Copyright © 2018 Steve Ravet. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
---
+++ glocraft_tabbedTests/glocraft_tabbedTests.swift
@@ -0,0 +1,34 @@
+//
+//  glocraft_tabbedTests.swift
+//  glocraft_tabbedTests
+//
+//  Created by Steve Ravet on 10/21/18.
+//  Copyright © 2018 Steve Ravet. All rights reserved.
+//
+
+import XCTest
+@testable import glocraft_tabbed
+
+class glocraft_tabbedTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
